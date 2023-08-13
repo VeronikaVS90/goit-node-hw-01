45fe09ddf4076a5cb4379d1431701d83341630ce
VeronikaVS90 <vsokolovanika@gmail.com>
2023-08-14 00:45:42 +0300

    step 2-3

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Welcome to the Node.js, friends")
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    switch (action) {
+        case "list":
+            const allContacts = await contacts.listContacts();
+            return console.table(allContacts);
+        case "getById":
+            const oneContact = await contacts.getById(id);
+            return console.log(oneContact);
+        case "add":
+            const newContact = await contacts.add(name, email, phone);
+            return console.log(newContact);
+        case "removeById":
+            const removeContact = await contacts.removeById(id);
+            return console.log(removeContact);
+    }
+}
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "getById", id: "AeHIrLTr6JkxGE6SN-0Rw" });
+// invokeAction({ action: "add", name: "Veronika", email: "vsokolovanika@gmail.com", phone: "(063) 454 5823" });
+invokeAction({ action: "removeById", id: "qdggE76Jtbfd9eWJHrssH"});
